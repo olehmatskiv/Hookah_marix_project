a2cd86065df0701db7c1f71ae43c0e5b36b1e3a6
olehmatskiv <85799629+olehmatskiv@users.noreply.github.com>
2022-06-05 16:32:22 +0300

    create templates

--- shop/views.py
+++ shop/views.py
@@ -3,4 +3,8 @@ from  django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Home<h1>')
+    return render(request , 'index.html')
+
+
+def about(request):
+    return render(request ,'about.html')
